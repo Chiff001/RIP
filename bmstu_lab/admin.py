2f306adef6069d4f7da076b38241fd7a48912f77
Chiff001 <74724374+Chiff001@users.noreply.github.com>
2024-12-18 15:21:11 +0300

    Delete bmstu_lab directory

--- bmstu_lab/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-
-admin.site.register(Personalities)
-admin.site.register(CompanyApp)
-admin.site.register(CompanyUser)
-# Register your models here.
